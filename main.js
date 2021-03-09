6fda930221dec76059f0e073f13e62e989920c7e
CaptainLKA <captainlka@macbook-pro-de-captainlka.home>
2021-03-09 13:23:07 +0100

    Update project html css js

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ window.addEventListener('load',function(){
         document.querySelector('.loader').classList.add("loaded");
     }, 1500);
 
-    /* Déclaration variables */
     const formulaire = document.getElementById('form');
     const email = document.getElementById('email');
     const infosubmit = document.getElementById('info-text');
 
-    /* Fonction de vérification avec regex */
     function validationEmail(email) {
         const result = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
@@ -20,7 +18,6 @@ window.addEventListener('load',function(){
         e.preventDefault();
         const emailValue = email.value;
 
-        /* Condition pour vérifier si l'email est valide */
         if(!validationEmail(emailValue)) {
             formulaire.classList.add('error');
             infosubmit.innerHTML = 'Please provide a valid email';
